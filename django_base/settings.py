ab7639099a02bc1478e18b209fc1605c4650176c
marioviti <vitimario2@gmail.com>
2017-08-06 16:54:48 +0200

    fixing auth

--- django_base/settings.py
+++ django_base/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_base'
+    'django.contrib.sites',
+    'app_base',
 ]
 
 MIDDLEWARE = [
